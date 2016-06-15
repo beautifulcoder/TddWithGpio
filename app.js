3a5fc2527705cb5f937a02630cfe3b6ac17d2eb5
Camilo Reyes <reyesc@gmail.com>
2016-06-14 19:43:38 -0500

    Finish blinker demo

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Hello, World!');
+var gpio = require('./gpio');
+
+gpio.open(16, 'out', function () {
+  var on = 0;
+
+  var blinker = setInterval(function () {
+    gpio.write(16, on, function () {
+      on = (on + 1) % 2;
+
+      console.log('ON = ' + on);
+    });
+  }, 1000);
+
+  setTimeout(function () {
+    clearInterval(blinker);
+  }, 12000);
+});
